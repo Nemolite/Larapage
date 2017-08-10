2862f3a1412ddae4e348a39ea69e8f8c601e0005
as7bzx92dd <g04032016@mail.ru>
2017-08-10 16:59:56 +0300

    db

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -1,15 +1,22 @@
 <?php
 namespace App\Http\Controllers;
+ use DB;
 
 use App\Http\Controllers\Controller;
 
 class testController extends Controller {
-	public function dis(){
-		echo __METHOD__;
-	}
 
-    public  function basaviwe() {
-        echo "test";
+	       public  function basaviwe() {
+//        DB::insert("INSERT INTO `ttts` (`name`,`airline`) VALUES (?,?)",['testets1','popopopopo']);
+        $colt = DB::update("UPDATE `ttts` SET `name` = 'sssss' WHERE id=:id",['id'=>3]);
+         echo  $colt;
+        $del= DB::delete("DELETE FROM `ttts` WHERE id=:id",['id'=>1]);
+          echo  $del;
+        $post = DB::select("SELECT * FROM `ttts`");
+
+        dump($post);
+
+
     }
 	
 }
