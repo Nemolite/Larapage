51907dd2cb853665d99028043a126659ef4c6e26
as7bzx92dd <g04032016@mail.ru>
2017-08-10 12:22:35 +0300

    basa2

---
+++ database/migrations/2017_08_10_080111_test.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Test extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number');
+            $table->string('email');
+            $table->string('name', 100);
+            $table->boolean('confirmed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('test');
+    }
+}
